a6de05ac60b2fa6b9846055dc789ec08ba0e1e30
Jacob <jacob.binnie@centrapass.com>
2023-02-12 16:51:41 +1300

    Refactor: RM ml-24 for now

--- components/topbar/index.tsx
+++ components/topbar/index.tsx
@@ -4,7 +4,7 @@ interface TopbarProps {}
 
 export const Topbar: React.FC<TopbarProps> = () => {
   return (
-    <div className="w-[calc(100%-6rem)] fixed top-0 left-0 h-24 ml-24 bg-gray-200 p-8 flex justify-between">
+    <div className="w-[calc(100%-6rem)] fixed top-0 left-0 h-24 bg-gray-200 p-8 flex justify-between">
       <div className="w-44 h-6 bg-gray-300 animate-pulse rounded"></div>
       <div className="w-44 h-6 bg-gray-300 animate-pulse rounded"></div>
       <div className="w-44 h-6 bg-gray-300 animate-pulse rounded"></div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -87,7 +87,7 @@ export default function Home() {
     <div className="flex overflow-hidden min-h-screen bg-gray-200">
       {/* <Sidebar /> */}
       <Topbar />
-      <div className="flex flex-col ml-24 mt-24 p-8 w-full items-center gap-5">
+      <div className="flex flex-col mt-24 p-8 w-full items-center gap-5">
         <h2 className="mt-4 mb-2 text-secondary">My Posts</h2>
         <CardContainer posts={myPosts} />
         {nearbyPosts.length > 0 && (
